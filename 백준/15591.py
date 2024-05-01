56ca012e9961923d9e0b64137fa2ae503529caf0
younggyo <lee4519698@hanmail.net>
2024-05-01 22:39:07 +0900

    15591 : MooTube(Silver)

---
+++ 백준/15591.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+N, Q = map(int, input().split())
+USADO = [[] for _ in range(N + 1)]
+
+# P(동영상 1), Q(동영상 2), R(USADO)
+for _ in range(N - 1):
+    p, q, r = map(int, input().split())
+    USADO[p].append((q, r))
+    USADO[q].append((p, r))
+
+# K(연관성), V(동영상 번호)
+for _ in range(Q):
+    k, v = map(int, input().split())
+    visited = [False] * (N + 1)
+    visited[v] = True
+    result = 0
+    queue = deque([(v, float("inf"))])
+
+    while queue:
+        v, r = queue.popleft()
+        for next_v, next_r in USADO[v]:
+            usado = min(r, next_r)
+            if usado >= k and not visited[next_v]:
+                result += 1
+                queue.append((next_v, next_r))
+                visited[next_v] = True
+    print(result)
