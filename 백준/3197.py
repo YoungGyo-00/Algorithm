97822a224abe545959187749e8f8b4d10146265e
younggyo <lee4519698@hanmail.net>
2024-06-01 16:07:39 +0900

    11401 : 이항 계수 3

--- 백준/3197.py
+++ 백준/3197.py
@@ -1,4 +1,7 @@
 from collections import deque
+import sys
+
+input = sys.stdin.readline
 
 r, c = map(int, input().split())
 
@@ -12,39 +15,43 @@ dy = [0, 1, 0, -1]
 pos = [(i, j) for i in range(r) for j in range(c) if arr[i][j] == "L"]  # 백조의 위치
 
 
-def bfs(map):
+def bfs(arr):
     q = deque()
     q.append((pos[0][0], pos[0][1]))
     visited = [[0] * c for _ in range(r)]
+    visited[pos[0][0]][pos[0][1]] = 1
     while q:
         x, y = q.popleft()
+        if x == pos[1][0] and y == pos[1][1]:
+            return 1
         for i in range(4):
             tx = x + dx[i]
             ty = y + dy[i]
-            if (
-                tx >= 0
-                and ty >= 0
-                and tx < r
-                and ty < c
-                and arr[tx][ty] == "."
-                and not visited[tx][ty]
-            ):
-                q.append((tx, ty))
+            if 0 <= tx < r and 0 <= ty < c and not visited[tx][ty]:
+                if arr[tx][ty] == "X":
+                    arr[tx][ty] = "."
+                else:
+                    q.append((tx, ty))
                 visited[tx][ty] = 1
-            if tx == pos[1][0] and ty == pos[1][1]:
-                return 1
     return 0
 
 
 def melt(arr):
-    water = [(i, j) for i in range(r) for j in range(c) if arr[i][j] == "."]  # 물 위치
-    while water:
-        x, y = water.pop()
+    q = deque()
+    q.append((pos[1][0], pos[1][1]))
+    visited = [[0] * c for _ in range(r)]
+    visited[pos[1][0]][pos[1][1]] = 1
+    while q:
+        x, y = q.pop()
         for i in range(4):
             tx = x + dx[i]
             ty = y + dy[i]
-            if tx >= 0 and ty >= 0 and tx < r and ty < c and arr[tx][ty] == "X":
-                arr[tx][ty] = "."
+            if 0 <= tx < r and 0 <= ty < c and not visited[tx][ty]:
+                if arr[tx][ty] == ".":
+                    q.append((tx, ty))
+                if arr[tx][ty] == "X":
+                    arr[tx][ty] = "."
+                visited[tx][ty] = 1
     return arr
 
 
