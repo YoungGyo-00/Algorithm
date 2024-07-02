0843ca53afef10b7b0e0e032c59db96737e8b71b
younggyo <lee4519698@hanmail.net>
2024-07-03 01:15:37 +0900

    1504  : 특정한 최단 경로

---
+++ 백준/1504.py
@@ -0,0 +1,41 @@
+# 특정한 최단 경로(그래프 이론)
+import heapq
+
+INF = 1e10
+n, e = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+
+# 양방향 간선
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
+
+x, y = map(int, input().split())
+
+
+def dijkstra(start):
+    global n, graph
+    dist = [INF] * (n + 1)
+    dist[start] = 0
+    q = []
+    heapq.heappush(q, (start, 0))
+
+    while q:
+        node, d = heapq.heappop(q)
+        for next_node, next_dist in graph[node]:
+            if next_dist + d < dist[next_node]:
+                dist[next_node] = next_dist + d
+                heapq.heappush(q, (next_node, next_dist + d))
+    return dist
+
+
+x_dist = dijkstra(x)
+y_dist = dijkstra(y)
+min_dist = min(x_dist[1] + x_dist[y] + y_dist[n], y_dist[1] + y_dist[x] + x_dist[n])
+
+if min_dist >= INF:
+    print(-1)
+else:
+    print(min_dist)
