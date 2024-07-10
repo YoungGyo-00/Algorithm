4d21bd16235c2c638fbb08de9f3aec0bf4bbd9ab
younggyo <lee4519698@hanmail.net>
2024-07-10 14:00:26 +0900

    15650 : N과 M(2)

---
+++ 백준/15650.py
@@ -0,0 +1,18 @@
+# N과 M(2)
+n, m = map(int, input().split())
+l = []
+
+
+def dfs(start):
+    if len(l) == m:
+        print(" ".join(map(str, l)))
+        return
+
+    for i in range(start, n + 1):
+        if i not in l:
+            l.append(i)
+            dfs(i + 1)
+            l.pop()
+
+
+dfs(1)
