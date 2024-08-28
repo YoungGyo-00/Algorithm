c42cb395be88eab0a079c47b3ee3f1f58cb7e712
younggyo <lee4519698@hanmail.net>
2024-08-28 19:40:51 +0900

    9466 : 텀 프로젝트

---
+++ BOJ/Graph/DFS&BFS/9466.py
@@ -0,0 +1,28 @@
+# 텀 프로젝트
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    selected = [0] + list(map(int, input().split()))
+
+    visited = [False] * (N + 1)
+    team_mems = 0
+
+    def dfs(i):
+        global team_mems
+
+        visited[i] = True
+        team.append(i)
+        select = selected[i]
+
+        if visited[select]:
+            if select in team:
+                team_mems += len(team[team.index(select) :])
+        else:
+            dfs(select)
+
+    for i in range(1, N + 1):
+        if not visited[i]:
+            team = []
+            dfs(i)
+
+    print(N - team_mems)
